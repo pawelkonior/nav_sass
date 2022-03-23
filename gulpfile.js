7c7118fc3c2b4a025b3a630ab29ed0ae526f0add
Paweł Konior <pawel.konior@gmail.com>
2022-03-23 12:47:35 +0100

    gulp configuration with 2 tasks and add script in package.json

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const entryPath = "./src";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+
+function compileSass(done) {
+    gulp
+        .src(entryPath + "/sass/main.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(entryPath + "/css"));
+
+    done();
+}
+
+function watcher(done) {
+    browserSync.init({
+        server: "./",
+    });
+
+    gulp.watch(entryPath + "/sass/**/*.scss", gulp.series(compileSass, reload));
+    gulp.watch("./*.html", gulp.series(reload));
+
+    done();
+}
+
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+exports.sass = gulp.parallel(compileSass);
+exports.default = gulp.parallel(compileSass, watcher);
